2fb8b057efbc1f2f26d2590c5072324da37ff46d
hoseintanavar <139155303+hoseintanavar@users.noreply.github.com>
2023-10-12 14:05:03 +0330

    first nav

---
+++ dentist/scripts/main.js
@@ -0,0 +1,15 @@
+// Add your initial JavaScript code here
+
+const navMenu = document.getElementById("navMenu");
+const navUl = document.getElementById("navUl");
+
+// fanction list
+
+
+
+
+// Eventlistner list
+
+navMenu.addEventListener("click" , () => {
+    navUl.classList.toggle("active");
+})
